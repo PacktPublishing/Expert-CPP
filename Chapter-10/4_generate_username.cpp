3aeb6b17344ad23b6b390bfca16e2ad9e93b77f4
Scott Hutchinson <scotthutchinson@usa.net>
2020-03-01 10:47:56 -0800

    Resolving compile-time errors
    Consider adding a random number implementation like https://stackoverflow.com/a/49717041/5652483

--- Chapter-10/4_generate_username.cpp
+++ Chapter-10/4_generate_username.cpp
@@ -1,6 +1,12 @@
-std::string generate_username(const std::string& email)
+int get_random_number() 
+{ 
+  // TODO: Implement random number generation.
+  return 1;
+}
+
+std::string generate_username(const std::string& email) 
 {
   int num = get_random_number();
   std::string local_part = email.substr(0, email.find('@'));
-  return local_part + num;
+  return local_part + std::to_string(num);
 }
