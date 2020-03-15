098ec0ad7f9b1db46b052626aece926e5715add9
wus1 <wus1@jhuapl.edu>
2020-03-14 23:36:16 -0400

    slightly changed in order to consistent with the text.

--- Chapter-4/ch4_4_class_template_explicit_inst.cpp
+++ Chapter-4/ch4_4_class_template_explicit_inst.cpp
@@ -9,17 +9,22 @@ struct A {
 	T val;
 };
 
-
-template <class T>   //T in this line is template parameter
-T A<T>::foo() {       //the 1st T refers to function return type, the T in <> specifies that this
-					  //function's template parameter is also the class template parameter
-	cout << val << endl;
+template <class T> //T in this line is template parameter
+T A<T>::foo() {    //the 1st T refers to function return type, the T in <> specifies that this
+				   //function's template parameter is also the class template parameter
 	return val;
 }
 
-extern template int A<int>::foo();
+extern template struct A<int>;
+#if 0               //set 1 rebuild it                
+int A<int>::foo() { //line A
+	return val+1;   //line B
+}                   //Line C
+#endif                                   
 
 int main(void) {
-	int x = A<int>(55).foo();
-	return x;
+	A<double> x(5);
+	A<int> y(5);
+	cout << "fD="<<x.foo() <<", fI="<< y.foo() << endl;
+	return 0;
 }
