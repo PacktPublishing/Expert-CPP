459363ccb8709a6c6aef52e36282e35ef07b020b
wus1 <wus1@jhuapl.edu>
2020-03-19 17:49:31 -0400

    slight change

--- Chapter-4/ch4_17_factorial_traditional.cpp
+++ Chapter-4/ch4_17_factorial_traditional.cpp
@@ -1,9 +1,15 @@
 //ch4_17_factorial_recursion.cpp
 #include <iostream>
-uint32_t f(const uint32_t n) {
-	if (n == 0) return 1;
-	return n * f(n - 1);
+uint32_t f1(const uint32_t n) {
+	return (n<=1) ? 1 : n * f1(n - 1);
 }
+
+constexpr uint32_t f2(const uint32_t n) {
+	return ( n<=1 )?  1 : n * f2(n - 1);
+}
+
 int main() {
-	std::cout << f(10) << std::endl;   //output 3628800
+	uint32_t a1 = f1(10);
+	const uint32_t a2 = f2(10);
+	std::cout << "a1=" << a1 << ", a2=" << a2  << std::endl;
 }
